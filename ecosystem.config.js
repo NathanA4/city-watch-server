cd15b82382a9040b7938b571a16d347bcb398b8d
Ethan Elliott <ethan.elliott@hotmail.ca>
2021-01-18 23:20:37 -0500

    adding actions

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    apps: [{
+        name: 'cw-server',
+        script: 'yarn start',
+        watch: false,
+        time: true,
+        autorestart: true,
+    }],
+    deploy: {
+        production: {
+            user: 'cw-deploy',
+            host: 'api.city-watch.ca',
+            key: 'deploy.key',
+            ref: 'origin/master',
+            repo: 'https://github.com/ethanelliott/city-watch-server',
+            path: '/var/www/production',
+            'post-deploy': 'yarn && yarn build && pm2 reload ecosystem.config.js --env production',
+        }
+    }
+};
